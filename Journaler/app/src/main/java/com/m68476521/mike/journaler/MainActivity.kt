ae6cd82b3f14067c2a685fc0f8d23a085f810263
Mike <m68476521.1@gmail.com>
2018-09-25 21:13:10 -0700

    Add ViewPager
    by m68476521

--- Journaler/app/src/main/java/com/m68476521/mike/journaler/MainActivity.kt
+++ Journaler/app/src/main/java/com/m68476521/mike/journaler/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.m68476521.mike.journaler
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override val tag = "Main activity"
@@ -10,10 +14,16 @@ class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val fragment = ItemsFragment()
-        supportFragmentManager
-                .beginTransaction()
-                .add(R.id.fragment_container, fragment)
-                .commit()
+        pager.adapter = ViewPagerAdapter(supportFragmentManager)
+    }
+
+    private class ViewPagerAdapter(manager: FragmentManager) : FragmentStatePagerAdapter(manager) {
+        override fun getItem(position: Int): Fragment {
+            return ItemsFragment()
+        }
+
+        override fun getCount(): Int {
+            return 5
+        }
     }
 }
--- Journaler/app/src/main/java/com/m68476521/mike/journaler/ManualFragment.kt
+++ Journaler/app/src/main/java/com/m68476521/mike/journaler/ManualFragment.kt
@@ -1,17 +1,6 @@
 package com.m68476521.mike.journaler
-import android.os.Bundle
 
 class ManualFragment: BaseFragment() {
     override val logTag = "Manual Fragment"
     override fun getLayout() = R.layout.fragment_manual
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val fragment = ItemsFragment()
-
-        fragmentManager!!
-                .beginTransaction()
-                .add(R.id.fragment_container, fragment)
-                .commit()
-    }
 }
