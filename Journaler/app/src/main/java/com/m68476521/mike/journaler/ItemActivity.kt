a513a98228aada32cfd9b01fd5f74d7f1ae6494f
Mike <m68476521.1@gmail.com>
2018-09-29 22:20:06 -0700

    Add alert that starts activities
    Start activities: TODO and NOTE from an Alert
    
    By m68476521

--- Journaler/app/src/main/java/com/m68476521/mike/journaler/ItemActivity.kt
+++ Journaler/app/src/main/java/com/m68476521/mike/journaler/ItemActivity.kt
@@ -1,3 +1,17 @@
 package com.m68476521.mike.journaler
 
-abstract class ItemActivity: BaseActivity() { }
+import android.os.Bundle
+import android.util.Log
+
+abstract class ItemActivity: BaseActivity() {
+    protected var mode = MODE.VIEW
+
+    override fun getActivityTitle() = R.string.app_name
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val modeTest = intent.getIntExtra(MODE.EXTRAS_KEY, MODE.VIEW.mode)
+        mode = MODE.getByValue(modeTest)
+        Log.v(tag, "Mode [ $mode ]")
+    }
+}
--- Journaler/app/src/main/java/com/m68476521/mike/journaler/ItemsFragment.kt
+++ Journaler/app/src/main/java/com/m68476521/mike/journaler/ItemsFragment.kt
@@ -1,7 +1,51 @@
 package com.m68476521.mike.journaler
 
+import android.app.AlertDialog
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
 class ItemsFragment : BaseFragment() {
     override val logTag = "Items fragment"
-    override fun getLayout(): Int {
-        return R.layout.fragment_items }
+    override fun getLayout() = R.layout.fragment_items
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater?.inflate(getLayout(), container, false)
+        val button = view?.findViewById<FloatingActionButton>(R.id.new_item)
+
+        button?.setOnClickListener {
+            val items = arrayOf(
+                    getString(R.string.todos),
+                    getString(R.string.notes)
+            )
+
+            val builder = AlertDialog.Builder(this@ItemsFragment.context)
+                    .setTitle(R.string.choose_a_type)
+                    .setItems(items) { _, which ->
+                        when (which) {
+                            0 -> openCreateNote()
+                            1 -> openCreateTodo()
+                            else -> Log.v(logTag, "Unknown option selected [ $which ]")
+                        }
+                    }
+            builder.show()
+        }
+        return view
+    }
+
+    private fun openCreateNote() {
+        val intent = Intent(context, NoteActivity::class.java)
+        intent.putExtra(MODE.EXTRAS_KEY, MODE.CREATE)
+        startActivity(intent)
+    }
+
+    private fun openCreateTodo() {
+        val intent = Intent(context, TodoActivity::class.java)
+        intent.putExtra(MODE.EXTRAS_KEY, MODE.CREATE)
+        startActivity(intent)
+    }
 }
