d02b5a1b83c60ba16494f55ae0865bdb67183413
Mike <m68476521.1@gmail.com>
2018-11-12 22:48:12 -0800

    Add Boot and shutdown receiver in Manifest
    by m68476521

---
+++ Journaler/app/src/main/java/com/m68476521/mike/journaler/receiver/ShutdownReceiver.kt
@@ -0,0 +1,11 @@
+package com.m68476521.mike.journaler.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class ShutdownReceiver: BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+
+    }
+}
