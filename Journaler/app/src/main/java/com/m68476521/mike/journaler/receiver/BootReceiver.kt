d6a1c643978971b101714b19f7180be0b992b859
Mike <m68476521.1@gmail.com>
2018-11-18 20:25:47 -0800

    Add Logs for BootReceiver and ShutdownReceiver
    by m68476521

--- Journaler/app/src/main/java/com/m68476521/mike/journaler/receiver/BootReceiver.kt
+++ Journaler/app/src/main/java/com/m68476521/mike/journaler/receiver/BootReceiver.kt
@@ -3,9 +3,11 @@ package com.m68476521.mike.journaler.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 
+private const val TAG = "Boot Receiver"
 class BootReceiver: BroadcastReceiver() {
     override fun onReceive(p0: Context?, p1: Intent?) {
-
+        Log.i(TAG, "Boot completed")
     }
 }
