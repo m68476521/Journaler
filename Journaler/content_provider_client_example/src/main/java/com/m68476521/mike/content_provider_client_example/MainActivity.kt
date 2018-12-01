d5d18ee037910345e41ff5f0383fd153a2f4806d
Mike <m68476521.1@gmail.com>
2018-11-30 22:48:39 -0800

    Insert by client app using content provider
    by m68476521

--- Journaler/content_provider_client_example/src/main/java/com/m68476521/mike/content_provider_client_example/MainActivity.kt
+++ Journaler/content_provider_client_example/src/main/java/com/m68476521/mike/content_provider_client_example/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.m68476521.mike.content_provider_client_example
 
+import android.content.ContentValues
 import android.location.Location
 import android.net.Uri
 import android.os.AsyncTask
@@ -46,6 +47,22 @@ class MainActivity : AppCompatActivity() {
             task.execute()
         }
         insert.setOnClickListener {
+            val task = object : AsyncTask<Unit, Unit, Unit>() {
+                override fun doInBackground(vararg p0: Unit?) {
+                    for (x in 0..5) {
+                        var uri = Uri.parse("content://com.journaler.provider/note")
+                        val values = ContentValues()
+                        values.put("title", "Title $x")
+                        values.put("mesagge", "Message $x")
+                        val location = Location("Stub location $x")
+                        location.latitude = x.toDouble()
+                        location.longitude = x.toDouble()
+                        values.put("location", gson.toJson(location))
+                        contentResolver.insert(uri, values)
+                    }
+                }
+            }
+            task.execute()
         }
         update.setOnClickListener {
         }
