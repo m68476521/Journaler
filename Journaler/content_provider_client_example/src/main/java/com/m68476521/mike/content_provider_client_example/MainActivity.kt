a9bf33c736d2c846a096b39ca587189c5910946d
Mike <m68476521.1@gmail.com>
2018-12-06 20:50:05 -0800

    Add delete notes into MainActivity
    by m68476521

--- Journaler/content_provider_client_example/src/main/java/com/m68476521/mike/content_provider_client_example/MainActivity.kt
+++ Journaler/content_provider_client_example/src/main/java/com/m68476521/mike/content_provider_client_example/MainActivity.kt
@@ -105,6 +105,26 @@ class MainActivity : AppCompatActivity() {
             task.execute()
         }
         delete.setOnClickListener {
+            val task = object : AsyncTask<Unit, Unit, Unit>() {
+                override fun doInBackground(vararg p0: Unit?) {
+                    val selection = StringBuilder()
+                    val selectionArgs = mutableListOf<String>()
+                    val uri = Uri.parse("content://com.journaler.provide/notes")
+                    val cursor = contentResolver.query(uri, null, selection.toString(),
+                            selectionArgs.toTypedArray(), null)
+                    while (cursor.moveToNext()) {
+                        val id = cursor.getLong(cursor.getColumnIndexOrThrow("_id"))
+                        val deleted = contentResolver.delete(uri, "_id = ?", arrayOf(id.toString()))
+                        if (deleted > 0) {
+                            Log.v(tag, "Notes deleted [ $deleted ]")
+                        } else {
+                            Log.e(tag, "Notes not deleted")
+                        }
+                    }
+                    cursor.close()
+                }
+            }
+            task.execute()
         }
     }
 }
