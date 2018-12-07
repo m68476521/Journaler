a53f1814e2a718dd00f5a0de4e3a241c72f0c483
Mike <m68476521.1@gmail.com>
2018-12-06 21:31:18 -0800

    Set up content loader
    by m68476521

--- Journaler/app/src/main/java/com/m68476521/mike/journaler/ItemsFragment.kt
+++ Journaler/app/src/main/java/com/m68476521/mike/journaler/ItemsFragment.kt
@@ -5,19 +5,23 @@ import android.animation.ObjectAnimator
 import android.app.Activity
 import android.app.AlertDialog
 import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
+import android.support.v4.app.LoaderManager
 import android.support.v4.content.ContextCompat
+import android.support.v4.content.CursorLoader
+import android.support.v4.content.Loader
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AccelerateInterpolator
 import android.view.animation.BounceInterpolator
-import android.widget.ListView
 import com.m68476521.mike.journaler.adapter.EntryAdapter
-import com.m68476521.mike.journaler.database.Content
-import com.m68476521.mike.journaler.execution.TaskExecutor
+import com.m68476521.mike.journaler.provider.JournalerProvider
+import kotlinx.android.synthetic.main.fragment_items.*
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -25,9 +29,37 @@ class ItemsFragment : BaseFragment() {
     private val TODO_REQUEST = 1
     private val NOTE_REQUEST = 0
     override val logTag = "Items fragment"
-    private val executor = TaskExecutor.getInstance(5)
+    private var adapter: EntryAdapter? = null
 
     override fun getLayout() = R.layout.fragment_items
+
+    private val loaderCallBack = object : LoaderManager.LoaderCallbacks<Cursor> {
+        override fun onLoadFinished(loader: Loader<Cursor>, cursor: Cursor?) {
+            cursor?.let {
+                if (adapter == null) {
+                    context?.let {
+                        adapter = EntryAdapter(it, cursor)
+                        items.adapter = adapter
+                    }
+                } else {
+                    adapter?.swapCursor(cursor)
+                }
+            }
+        }
+
+        override fun onLoaderReset(p0: Loader<Cursor>) {
+            adapter?.swapCursor(null)
+        }
+
+        override fun onCreateLoader(p0: Int, p1: Bundle?): Loader<Cursor> {
+            return CursorLoader(context!!, Uri.parse(JournalerProvider.URL_NOTES), null, null, null, null)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        loaderManager.initLoader(0, null, loaderCallBack)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater?.inflate(getLayout(), container, false)
@@ -97,18 +129,30 @@ class ItemsFragment : BaseFragment() {
     }
 
     private fun animate(button: FloatingActionButton, expand: Boolean = true) {
-       val animation1 = ObjectAnimator.ofFloat(button, "scaleX",
-               if (expand) { 1.5f } else { 1.0f})
+        val animation1 = ObjectAnimator.ofFloat(button, "scaleX",
+                if (expand) {
+                    1.5f
+                } else {
+                    1.0f
+                })
         animation1.duration = 2000
         animation1.interpolator = BounceInterpolator()
 
         val animation2 = ObjectAnimator.ofFloat(button, "scaleY",
-                if (expand) { 1.5f} else { 1.0f})
+                if (expand) {
+                    1.5f
+                } else {
+                    1.0f
+                })
         animation2.duration = 2000
         animation2.interpolator = BounceInterpolator()
 
         val animation3 = ObjectAnimator.ofFloat(button, "alpha",
-                if (expand) { 0.3f} else { 1.0f })
+                if (expand) {
+                    0.3f
+                } else {
+                    1.0f
+                })
         animation3.duration = 500
         animation3.interpolator = AccelerateInterpolator()
 
@@ -119,24 +163,8 @@ class ItemsFragment : BaseFragment() {
 
     override fun onResume() {
         super.onResume()
+        loaderManager.restartLoader(0, null, loaderCallBack)
         val btn = view?.findViewById<FloatingActionButton>(R.id.new_item)
         btn?.let { animate(btn, false) }
-
-        executor.execute {
-            val notes = Content.NOTE.selectAll()
-            val adapter = EntryAdapter(this.activity!!, notes)
-            activity?.runOnUiThread {
-                view?.findViewById<ListView>(R.id.items)?.adapter = adapter
-            }
-        }
-
-        val items = view?.findViewById<ListView>(R.id.items)
-        items?.let {
-            items.postDelayed({
-                if (!activity!!.isFinishing) {
-                    items.setBackgroundColor(ContextCompat.getColor(context!!, R.color.grey_text_middle))
-                }
-            }, 3000)
-        }
     }
 }
